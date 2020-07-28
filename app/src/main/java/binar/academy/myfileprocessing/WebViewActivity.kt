ff4bac9eddf86e15abdf07dedc9d38ee2513b6e9
Abika Chairul Yusri <35727186+abikayusri@users.noreply.github.com>
2020-07-28 11:08:42 +0700

    Finish Work

--- app/src/main/java/binar/academy/myfileprocessing/WebViewActivity.kt
+++ app/src/main/java/binar/academy/myfileprocessing/WebViewActivity.kt
@@ -2,10 +2,19 @@ package binar.academy.myfileprocessing
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebViewClient
+import binar.academy.myfileprocessing.utils.Utils
+import kotlinx.android.synthetic.main.activity_web_view.*
 
 class WebViewActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web_view)
+
+        webView.webViewClient = WebViewClient()
+        webView.settings.setSupportZoom(true)
+        webView.settings.javaScriptEnabled = true
+        val url = Utils.getPdfUrl()
+        webView.loadUrl("https://docs.google.com/gview?embedded=true&url=$url")
     }
 }
